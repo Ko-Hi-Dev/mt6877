644d5edac102738a863c010f854c94d27cf00226
Kohi <nihonnofanboi@gmail.com>
2024-05-20 22:24:41 +0200

    keymaster

--- ossi.mk
+++ ossi.mk
@@ -207,9 +207,6 @@ PRODUCT_PACKAGES += \
    libkeystore-wifi-hidl \
    libkeystore-engine-wifi-hidl
 
-PRODUCT_PACKAGES += \
-$(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.keymaster@4.1.so
-
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.mt6877
