93ece9b19846e8e658265a47450e5c685f9d6cee
Kohi <nihonnofanboi@gmail.com>
2024-05-20 13:27:51 +0200

    revert

--- ossi.mk
+++ ossi.mk
@@ -124,10 +124,19 @@ SYSTEMUI_OPTIMIZE_JAVA := true
 
 # Display
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0.vendor \
+    android.hardware.graphics.allocator@3.0.vendor \
     android.hardware.graphics.allocator@4.0.vendor \
-    android.hardware.graphics.composer@2.3-service \
+    android.hardware.graphics.composer@2.1-resources.vendor \
+    android.hardware.graphics.composer@2.2-resources.vendor \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    android.hardware.graphics.composer@2.3-service \
+    android.hidl.allocator@1.0.vendor \
+    android.hardware.memtrack-service.mediatek-mali \
+    android.hardware.graphics.common-V2-ndk_platform.vendor \
+    android.hardware.graphics.common-V2-ndk.vendor \
+    disable_configstore
 
 # Display saturation adjust
 PRODUCT_VENDOR_PROPERTIES += \
