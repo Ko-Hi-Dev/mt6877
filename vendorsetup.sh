a42f57e8db378377310ed604c362ec01c0ecce7f
Kohi <nihonnofanboi@gmail.com>
2024-05-21 22:59:12 +0200

    auto clone

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,12 +4,11 @@
 KSU=kernel/oplus/mt6877/KernelSU
 rm -rf $KSU && git clone https://github.com/tiann/KernelSU $KSU
 
+git clone https://github.com/Ko-Hi-Dev/ivan.git device/oplus/ivan
+
 git clone https://gitlab.com/Ko_Hi/mt6877_vendor.git vendor/oplus/mt6877-common
 
 git clone https://gitlab.com/Ko_Hi/mt6877.git vendor/oplus/ivan
-
-# Source patches
-bash device/oplus/mt6877-common/patches/apply.sh
 
 git clone https://github.com/oplus-ossi-development/android_device_mediatek_sepolicy_vndr.git device/mediatek/seploicy_vndr
 
@@ -25,3 +24,8 @@ git clone https://github.com/oplus-ossi-development/android_hardware_mediatek_wl
 
 # clang
 git clone --depth=1 https://github.com/oneplus-mediatek-dev/android_prebuilts_clang_host_linux-x86_clang-6443078.git -b 11.0.1 prebuilts/clang/host/linux-x86/clang-r383902
+
+
+# Source patches
+bash device/oplus/mt6877-common/patches/apply.sh
+
