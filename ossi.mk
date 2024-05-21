d8ac6c5189904b78c656c719d497e7bae9770890
Kohi <nihonnofanboi@gmail.com>
2024-05-21 22:19:07 +0200

    readd

--- ossi.mk
+++ ossi.mk
@@ -405,10 +405,6 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl:64 \
     android.hardware.thermal@2.0.vendor:64
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-mediatekv2
