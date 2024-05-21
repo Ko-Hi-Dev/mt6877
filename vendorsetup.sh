ac219c1f7cb07d84a94d123c4abcc38e50239fa8
Ko-Hi-Dev <60625753+Ko-Hi-Dev@users.noreply.github.com>
2024-05-22 01:37:08 +0200

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,8 +10,6 @@ git clone https://gitlab.com/Ko_Hi/mt6877_vendor.git vendor/oplus/mt6877-common
 
 git clone https://gitlab.com/Ko_Hi/mt6877.git vendor/oplus/ivan
 
-git clone https://github.com/oplus-ossi-development/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
-
 git clone -b lineage-21 https://github.com/LineageOS/android_hardware_oplus.git hardware/oplus
 
 git clone https://github.com/oplus-ossi-development/android_hardware_mediatek.git hardware/mediatek
@@ -21,6 +19,9 @@ git clone https://github.com/oplus-ossi-development/android_packages_apps_Oneplu
 git clone https://github.com/oplus-ossi-development/android_hardware_mediatek_gnss.git hardware/mediatek/gnss
 
 git clone https://github.com/oplus-ossi-development/android_hardware_mediatek_wlan.git hardware/mediatek/wlan
+
+git clone -b lineage-21 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git device/mediatek/sepolicy_vndr
+
 
 # clang
 git clone --depth=1 https://github.com/oneplus-mediatek-dev/android_prebuilts_clang_host_linux-x86_clang-6443078.git -b 11.0.1 prebuilts/clang/host/linux-x86/clang-r383902
