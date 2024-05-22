1816a20acfc8d7bb9d80ec21a15357db239dd7bf
Kohi <nihonnofanboi@gmail.com>
2024-05-22 14:45:25 +0200

    more sepolicy

--- ossi.mk
+++ ossi.mk
@@ -16,6 +16,9 @@
 
 COMMON_PATH := device/oplus/mt6877-common
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
