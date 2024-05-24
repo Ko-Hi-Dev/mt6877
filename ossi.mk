6a04746094f45682519b4334144d5c9bf7c80b04
Kohi <nihonnofanboi@gmail.com>
2024-05-24 17:19:12 +0200

    revert

--- ossi.mk
+++ ossi.mk
@@ -368,8 +368,11 @@ PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.mtkpower@1.2.vendor
 
 # Perf
+PRODUCT_PACKAGES += \
+    libqti-perfd-client
+
+# Perf
 PRODUCT_COPY_FILES += \
-    libqti-perfd-client \
     system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     $(COMMON_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
 
