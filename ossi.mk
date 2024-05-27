27a517504318cc26b7add676094ad33bf1970e1f
Kohi <nihonnofanboi@gmail.com>
2024-05-27 10:20:46 +0200

    manif

--- ossi.mk
+++ ossi.mk
@@ -209,10 +209,6 @@ PRODUCT_PACKAGES += \
    libkeystore-wifi-hidl \
    libkeystore-engine-wifi-hidl
 
-# KProfiles
-PRODUCT_PACKAGES += \
-    KProfiles
-
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.mt6877
