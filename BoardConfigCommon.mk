4529711deadd30c31f455f790d4dbf9e7e42dc88
Kohi <nihonnofanboi@gmail.com>
2024-05-27 19:02:09 +0200

    1.1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,12 +71,12 @@ BUILD_BROKEN_INCORRECT_PARTITION_IMAGES := true
 
 
 # NFC
-DEVICE_MANIFEST_SKUS += nfc
-DEVICE_MANIFEST_NFC_FILES := $(COMMON_PATH)/configs/vintf/manifest_nfc.xml
+#DEVICE_MANIFEST_SKUS += nfc
+#DEVICE_MANIFEST_NFC_FILES := $(COMMON_PATH)/configs/vintf/manifest_nfc.xml
 
 # Compatibility matrix
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/framework_compatibility_matrix.xml
-DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
+#DEVICE_MATRIX_FILE := $(COMMON_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/vintf/manifest.xml
 
 # Display
