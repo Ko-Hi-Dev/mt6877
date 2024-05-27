d229de37ab4d7ee414a20f19af276796b35e7665
Kohi <nihonnofanboi@gmail.com>
2024-05-27 15:07:05 +0200

    .

--- ossi.mk
+++ ossi.mk
@@ -373,11 +373,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-mediatek \
     android.hardware.power@1.3.vendor \
-    android.hardware.power@2.0.vendor
 
 PRODUCT_PACKAGES += \
-    vendor.mediatek.hardware.mtkpower@1.2.vendor \
-    vendor.mediatek.hardware.mtkpower@2.0.vendor
+    vendor.mediatek.hardware.mtkpower@1.2.vendor 
 
 # Perf
 PRODUCT_COPY_FILES += \
