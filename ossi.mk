b6d88b46dfd02e7d910eff4d80b047683adaa1b7
Kohi <nihonnofanboi@gmail.com>
2024-05-28 16:40:15 +0200

    thermal

--- ossi.mk
+++ ossi.mk
@@ -495,7 +495,6 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.mtk \
     android.hardware.thermal@2.0.vendor \
     android.hardware.thermal@1.0-impl
 
