ce8475fef83e0b4f8600167678aa7f1764cdbb6b
Kohi <nihonnofanboi@gmail.com>
2024-05-30 12:23:54 +0200

    fix props

--- ossi.mk
+++ ossi.mk
@@ -111,9 +111,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4.vendor \
-    libdrmclearkeyplugin.vendor \
-    libmockdrmcryptoplugin.vendor \
-    libclearkeycasplugin.vendor \
     libdrm.vendor \
     libdrm
 
