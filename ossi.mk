fecd552a100ab49e133a8fa780289c8a365f78e7
Kohi <nihonnofanboi@gmail.com>
2024-06-09 17:35:56 +0200

    nfc

--- ossi.mk
+++ ossi.mk
@@ -269,6 +269,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
+    android.hardware.nfc_snxxx@1.2-service \
     android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     NfcNci \
