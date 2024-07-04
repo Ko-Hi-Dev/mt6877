a674ef6ae9f70c74a7c1f0b8b19fcb1ae59ce32f
Kohi <nihonnofanboi@gmail.com>
2024-07-04 19:36:41 +0200

    update fp overlays

--- ossi.mk
+++ ossi.mk
@@ -306,7 +306,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage \
     $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
