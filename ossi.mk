58e890d39d5d78416463a8bddb853f53d08b5b91
Kohi <nihonnofanboi@gmail.com>
2024-07-05 18:06:56 +0200

    update fastcharge files and update for upcoming changes

--- ossi.mk
+++ ossi.mk
@@ -157,6 +157,10 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fastcharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.mt6877
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-service \
