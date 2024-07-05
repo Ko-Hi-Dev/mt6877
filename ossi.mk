8ede3bfae5f56ea471f0d0ffe13ece4cbe88fc92
Kohi <nihonnofanboi@gmail.com>
2024-07-05 19:59:47 +0200

    OplusExtras

--- ossi.mk
+++ ossi.mk
@@ -300,6 +300,14 @@ PRODUCT_PACKAGES += \
     android.hardware.usb@1.3.vendor \
     android.hardware.usb.gadget@1.0.vendor \
     android.hardware.usb.gadget@1.1.vendor
+    
+# OPlusExtras
+PRODUCT_PACKAGES += \   
+    OPlusExtras 
+    
+# Overlays
+PRODUCT_PACKAGES += \
+    OPlusExtrasResTarget
 
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
